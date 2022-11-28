7cde470a0adb1a4e02bbf8387f807c8e946128a6
GitCat3 <hulkbuster119@gmail.com>
2022-11-28 06:07:40 -0500

    added fancygradle, still not working

--- src/main/java/me/give_me_moneyz/electroaddon/ElectroAddon.java
+++ src/main/java/me/give_me_moneyz/electroaddon/ElectroAddon.java
@@ -12,7 +12,7 @@ public class ElectroAddon {
 
     public static final String MOD_ID = "electroaddon";
     public static final String MOD_NAME = "Electro Addon";
-    public static final String VERSION = "1.0-SNAPSHOT";
+    public static final String VERSION = "1.0";
 
     /**
      * This is the instance of your mod as created by Forge. It will never be null.
